178daa2b8d306c33d3bc1be1327e33e27ca36583
Chris Keefe <crk239@nau.edu>
2020-10-29 18:25:45 -0700

    SQUASH

--- lloyds_seq/lloyds_seq.c
+++ lloyds_seq/lloyds_seq.c
@@ -2,6 +2,18 @@
 #include<stdlib.h>
 #include<math.h>
 
+// Program should take K and then a data set
 int main(int argc, char *argv[]){
-    printf("Hello world!\n");
+printf("%d", argc);
+
+// TODO: bump this up to 3 when we're ready to deal with data
+if(argc != 2){
+    printf("Incorrect number of args. Should be 2, received %d\n", argc - 1);
+    exit(1);
+}
+
+int K = atoi(argv[1]);
+int clustering[K];
+
+exit 0;
 }
