82402c119042b556ce52f8bbae01da0b888d0964
Chris Keefe <39198770+ChrisKeefe@users.noreply.github.com>
2020-10-29 20:39:52 -0700

    CSV reader and CLI basics for Sequential LLoyd's (#1)
    * C boilerplate
    
    * SQUASH
    
    * Adds csvparser and iris data
    
    * Our code reads csvs
    
    * Reads data dims, drops labels, declares an array for our data
    
    * updates README for sequential LLoyd's

---
+++ lloyds_seq/lloyds_seq.c
@@ -0,0 +1,58 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include "csvparser.h"
+
+// Program should take K, a data set (.csv), a delimiter,
+// a binary flag data_contains_header, and a binary flag to drop labels
+int main(int argc, char *argv[]){
+  CsvParser *reader;
+  CsvRow *row;
+
+  if(argc != 6){
+      printf("Incorrect number of args. Should be 5, received %d\n", argc - 1);
+      exit(1);
+  }
+
+  int K = atoi(argv[1]);
+  int clustering[K];
+  char *data_fp = argv[2];
+  char *delimiter = argv[3];
+  int has_header_row = atoi(argv[4]);
+  int drop_labels = atoi(argv[5]);
+
+  // Take in data set
+  reader = CsvParser_new(data_fp, delimiter, has_header_row);
+  
+  // Get number of columns
+  row = CsvParser_getRow(reader);
+  int num_cols = CsvParser_getNumFields(row);
+  if (drop_labels){
+    num_cols--;
+  }
+
+  // Get number of rows like lazy people
+  int num_rows = 1;
+  while ((row = CsvParser_getRow(reader))){
+    num_rows++;
+  }
+
+  printf("Num cols: %d\tNum rows: %d\n", num_cols, num_rows);
+
+  float data_matrix[num_rows][num_cols];
+
+  // Torch the CsvParser and start again so we can read data in.
+  CsvParser_destroy(reader);
+
+  // Initialize some cluster centers from random rows in our data
+
+  // Assign points to cluster centers
+    // calculate within-cluster sum of squares
+    // total within-cluster sum of squares
+  
+  // break out of loop if total within-cluster sum of squares has converged
+
+  // Find cluster means and reassign centers
+
+  exit(0);
+}
