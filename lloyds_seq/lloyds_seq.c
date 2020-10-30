b5459d889af49d1f5887c56959a6a7d1a14484e7
Chris Keefe <crk239@nau.edu>
2020-10-29 19:32:51 -0700

    Our code reads csvs

--- lloyds_seq/lloyds_seq.c
+++ lloyds_seq/lloyds_seq.c
@@ -1,19 +1,33 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<math.h>
+#include "csvparser.h"
 
-// Program should take K and then a data set
+// Program should take K, a data set (.csv), a delimiter,
+// and a binary flag data_contains_header
 int main(int argc, char *argv[]){
-printf("%d", argc);
+  CsvParser *reader;
+  CsvRow *row;
+  const CsvRow *header;
 
-// TODO: bump this up to 3 when we're ready to deal with data
-if(argc != 2){
-    printf("Incorrect number of args. Should be 2, received %d\n", argc - 1);
-    exit(1);
-}
+  printf("%d\n", argc);
+  if(argc != 5){
+      printf("Incorrect number of args. Should be 4, received %d\n", argc - 1);
+      exit(1);
+  }
 
-int K = atoi(argv[1]);
-int clustering[K];
+  int K = atoi(argv[1]);
+  int clustering[K];
+  char *delimiter = argv[3];
+  int has_header_row = atoi(argv[4]);
 
-exit 0;
+  // Take in data set
+  reader = CsvParser_new(argv[2], delimiter, has_header_row);
+  header = CsvParser_getHeader(reader);
+
+  const char **headerFields = CsvParser_getFields(header);
+  printf("%s\n", headerFields[0]);
+
+
+  exit(0);
 }
