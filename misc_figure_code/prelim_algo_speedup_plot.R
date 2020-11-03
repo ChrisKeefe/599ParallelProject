c66e816838f1a55cc3eab704cb020985ae01d23f
Chris Keefe <crk239@nau.edu>
2020-11-02 18:33:09 -0700

    Updates speedup plot

--- misc_figure_code/prelim_algo_speedup_plot.R
+++ misc_figure_code/prelim_algo_speedup_plot.R
@@ -1,7 +1,10 @@
-datas <- data.table("5"=c(8.722372, 5.371310, 1.623881),
-                    "10"=c(27.166801, 17.185837, 1.580766),
-                    "15"=c(140.110077, 96.897912, 1.445955),
-                    "25"=c(453.748933, 312.411563, 1.452407),
+library(data.table)
+library(ggplot2)
+
+datas <- data.table("5"=c(8.722372, 3.737203, 2.333930),
+                    "10"=c(27.166801, 10.894545, 2.493615),
+                    "15"=c(140.110077, 58.985418, 2.375334),
+                    "25"=c(453.748933, 208.646101, 2.174729),
                     "label"=c("Serial", "Parallel", "Speedup"))
 
 data.long <- melt(datas, id.vars="label", measure.vars=c("5", "10", "15", "25"),
@@ -13,12 +16,11 @@ names(data.times) <- c("Key", "K Centroids", "Time (S)")
 
 (data.speedup <- data.long[label=="Speedup"])
 names(data.speedup)[3] <- "Speedup"
-data.speedup
 
 ggplot() +
   geom_line(aes(x=`K Centroids`, y=`Time (S)`, color=Key),
             data=data.times) +
-  geom_line(aes(x=`K Centroids`, y=(Speedup-1)*400, color=label), data=data.speedup) +
-  scale_y_continuous(sec.axis = sec_axis(trans=~(./400)+1, name="Speedup")) +
+  geom_line(aes(x=`K Centroids`, y=(Speedup-2)*400, color=label), data=data.speedup) +
+  scale_y_continuous(sec.axis = sec_axis(trans=~(./400)+2, name="Speedup")) +
   theme(axis.title.y.right = element_text(angle=0, vjust = .5),
         axis.title.y.left = element_text(angle=0, vjust = .5))
