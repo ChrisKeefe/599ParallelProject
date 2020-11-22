599459f7a18485db41576260d689a8151136076d
Anthony Simard <als872@nau.edu>
2020-11-22 12:04:14 -0700

    change scripts

--- elkan_cuda/jobscripts/elkan_cuda_time.sh
+++ elkan_cuda/jobscripts/elkan_cuda_time.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=Q3
-#SBATCH --output=/scratch/crk239/599ParallelProj/elkan_cuda_out.txt
+#SBATCH --job-name=e_cu_time
+#SBATCH --output=/scratch/als872/599ParallelProj/elkan_cuda_time.txt
 #SBATCH --time=5:00
-#SBATCH --chdir=/scratch/crk239/599ParallelProj/elkan_cuda
 #SBATCH --mem=10000
 #SBATCH -G 1
 #SBATCH --constraint=p100
-#SBATCH --account=cs450-fall20
 #SBATCH --qos=gpu_class
 
 # load a module, for example
 module load cuda
 
+nvcc -arch=compute_60 -code=sm_60 -lcuda -Xcompiler -fopenmp elkan_cuda.cu -o elkan_cuda
 # run
 ./elkan_cuda 5 "../data/pairs.csv" "," 0 0
 ./elkan_cuda 10 "../data/pairs.csv" "," 0 0
