4d196b652eb08a6b73c3f9a9a7f28f91ba4300c5
Anthony Simard <als872@nau.edu>
2020-11-22 14:29:02 -0700

    typos

--- elkan_cuda/jobscripts/elkan_cuda_time.sh
+++ elkan_cuda/jobscripts/elkan_cuda_time.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --job-name=l_cu_time
-#SBATCH --output=/scratch/als872/ekan_cuda_out.txt
+#SBATCH --output=/scratch/als872/elkan_cuda_out.txt
 #SBATCH --time=5:00
 #SBATCH --mem=10000
 #SBATCH -G 1
@@ -10,10 +10,10 @@
 # load a module, for example
 module load cuda
 
-nvcc -arch=compute_60 -code=sm_60 -lcuda -Xcompiler -fopenmp ekan_cuda.cu csvparser.c -o ekan_cuda
+nvcc -arch=compute_60 -code=sm_60 -lcuda -Xcompiler -fopenmp elkan_cuda.cu csvparser.c -o elkan_cuda
 
 # run
-srun ./ekan_cuda 5 "../data/pairs.csv" "," 0 0
-srun ./ekan_cuda 10 "../data/pairs.csv" "," 0 0
-srun ./ekan_cuda 15 "../data/pairs.csv" "," 0 0
-srun ./ekan_cuda 25 "../data/pairs.csv" "," 0 0
+srun ./elkan_cuda 5 "../data/pairs.csv" "," 0 0
+srun ./elkan_cuda 10 "../data/pairs.csv" "," 0 0
+srun ./elkan_cuda 15 "../data/pairs.csv" "," 0 0
+srun ./elkan_cuda 25 "../data/pairs.csv" "," 0 0
