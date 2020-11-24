113f3169f65aeb0d8cb705b674c7282d0d523cf3
Anthony Simard <als872@nau.edu>
2020-11-24 10:35:54 -0700

    modify sh

--- elkan_cuda/jobscripts/elkan_cuda_test.sh
+++ elkan_cuda/jobscripts/elkan_cuda_test.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
-#SBATCH --job-name=Q3
-#SBATCH --output=/scratch/crk239/599ParallelProj/elkan_cuda_out.txt
+#SBATCH --job-name=e_cu_time
+#SBATCH --output=/scratch/als872/elkan_cuda_out.txt
 #SBATCH --time=5:00
-#SBATCH --chdir=/scratch/crk239/599ParallelProj/elkan_cuda
 #SBATCH --mem=10000
 #SBATCH -G 1
 #SBATCH --constraint=p100
-#SBATCH --account=cs450-fall20
 #SBATCH --qos=gpu_class
 
 # load a module, for example
 module load cuda
 
+nvcc -arch=compute_60 -code=sm_60 -lcuda -Xcompiler -fopenmp elkan_cuda.cu csvparser.c -o elkan_cuda
+
 # run
-# The final argument triggers run in debug mode with our preselected iris centers
-test: lloyds_seq
-	./elkan_cuda 3 "../data/iris.csv" "," 1 1 1
+srun /elkan_cuda 3 "../data/iris.csv" "," 1 1 1
