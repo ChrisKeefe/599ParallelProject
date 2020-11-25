f038ed539fc8cc0f66d75c459ff86179b3cf8b6f
ChrisKeefe <crk239@nau.edu>
2020-11-25 14:25:07 -0700

    Fixes outputs for non_cuda timings

--- non_cuda_time.sh
+++ non_cuda_time.sh
@@ -19,15 +19,15 @@
 ./lloyds_slow_par/lloyds_slow_par 15 "data/pairs.csv" "," 0 0 >> lloyds_slow_par_out.txt
 ./lloyds_slow_par/lloyds_slow_par 25 "data/pairs.csv" "," 0 0 >> lloyds_slow_par_out.txt
 
-./lloyds_seq/lloyds_seq 5 "data/pairs.csv" "," 0 0 >> elkan_seq_out.txt
-./lloyds_seq/lloyds_seq 10 "data/pairs.csv" "," 0 0 >> elkan_seq_out.txt
-./lloyds_seq/lloyds_seq 15 "data/pairs.csv" "," 0 0 >> elkan_seq_out.txt
-./lloyds_seq/lloyds_seq 25 "data/pairs.csv" "," 0 0 >> elkan_seq_out.txt
+./lloyds_seq/lloyds_seq 5 "data/pairs.csv" "," 0 0 >> lloyds_seq_out.txt
+./lloyds_seq/lloyds_seq 10 "data/pairs.csv" "," 0 0 >> lloyds_seq_out.txt
+./lloyds_seq/lloyds_seq 15 "data/pairs.csv" "," 0 0 >> lloyds_seq_out.txt
+./lloyds_seq/lloyds_seq 25 "data/pairs.csv" "," 0 0 >> lloyds_seq_out.txt
 
-./lloyds_par/lloyds_par 5 "data/pairs.csv" "," 0 0 >> elkan_par_out.txt
-./lloyds_par/lloyds_par 10 "data/pairs.csv" "," 0 0 >> elkan_par_out.txt
-./lloyds_par/lloyds_par 15 "data/pairs.csv" "," 0 0 >> elkan_par_out.txt
-./lloyds_par/lloyds_par 25 "data/pairs.csv" "," 0 0 >> elkan_par_out.txt
+./lloyds_par/lloyds_par 5 "data/pairs.csv" "," 0 0 >> lloyds_par_out.txt
+./lloyds_par/lloyds_par 10 "data/pairs.csv" "," 0 0 >> lloyds_par_out.txt
+./lloyds_par/lloyds_par 15 "data/pairs.csv" "," 0 0 >> lloyds_par_out.txt
+./lloyds_par/lloyds_par 25 "data/pairs.csv" "," 0 0 >> lloyds_par_out.txt
 
 ./elkan_seq/elkan_seq 5 "data/pairs.csv" "," 0 0 >> elkan_seq_out.txt
 ./elkan_seq/elkan_seq 10 "data/pairs.csv" "," 0 0 >> elkan_seq_out.txt
